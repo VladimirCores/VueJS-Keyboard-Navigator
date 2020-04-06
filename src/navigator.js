8861c2dbf99556c8acf084b733264b5d330e2d63
Vladimir Minkin <Vladimir.Minkin@massive.co>
2020-04-06 22:37:34 +0200

    Update for Navigator subscribe/unsubscribe Minor improvements

--- src/navigator.js
+++ src/navigator.js
@@ -1,6 +1,6 @@
 let _selectedDom
 let _elementsCount
-let _callbacks = new Map()
+const _callbacks = new Map()
 
 function findElement (id, parentElement) { // level can be removed
 	if (!parentElement) return null
@@ -26,6 +26,7 @@ class Element {
 		this.inArray = false
 		this.first = false
 		this.last = false
+		this.dom = null
 	}
 }
 
@@ -48,45 +49,42 @@ class Navigator {
 			if (_callbacks.has(e)) _callbacks.get(e).push(c)
 			else _callbacks.set(e, [c])
 		}
-		console.log('events instanceof String', typeof events)
-		if (events instanceof Array)
-			events.forEach((e) => mapCallbackToEvent(e, callback))
-		else if (typeof(events) === 'string' || events instanceof String)
+		if (events instanceof Array) {
+			events.forEach(e => mapCallbackToEvent(e, callback))
+		} else if (typeof (events) === 'string' || events instanceof String) {
 			mapCallbackToEvent(events, callback)
-		else
+		} else {
 			mapCallbackToEvent(Navigator.EVENT_ANY, callback)
+		}
 	}
 	
 	static unsubscribe (callback, events) {
 		const removeCallback = (e, c) => {
-			if (_callbacks.has(e))
-				if (c) _callbacks.get(e).splice(_callbacks[e].indexOf(c), 1)
-				else _callbacks.delete(e)
+			if (_callbacks.has(e)) {
+				if (c) {
+					const methods = _callbacks.get(e)
+					methods.splice(methods.indexOf(c), 1)
+				} else _callbacks.delete(e)
+			}
 		}
-		if (typeof(callback) === 'string' || callback instanceof String) {
+		if (typeof (callback) === 'string' || callback instanceof String) {
 			removeCallback(callback)
-		} else if (callback instanceof Array)
-			callback.forEach((e) => removeCallback(e))
+		} else if (callback instanceof Array) callback.forEach((e) => removeCallback(e))
 		else {
 			if (events) {
-				if (events instanceof Array)
-					events.forEach((e) => removeCallback(e, callback))
-				else if (typeof(events) === 'string' || events instanceof String)
+				if (events instanceof Array) events.forEach((e) => removeCallback(e, callback))
+				else if (typeof (events) === 'string' || events instanceof String) {
 					removeCallback(events, callback)
-			} else
-				removeCallback(Navigator.EVENT_ANY, callback)
+				}
+			} else removeCallback(Navigator.EVENT_ANY, callback)
 		}
 	}
 	
 	static findElementByID (id) {
 		let element
 		if (id) {
-			if (Navigator.idToElement.has(id)) { // look in cache
-				element = Navigator.idToElement.get(id)
-			} else {
-				element = findElement(id, Navigator.tree)
-				Navigator.idToElement.set(id, element) // cache
-			}
+			if (Navigator.idToElement.has(id)) element = Navigator.idToElement.get(id) // look in cache
+			else Navigator.idToElement.set(id, (element = findElement(id, Navigator.tree))) // cache
 		}
 		return element
 	}
@@ -100,6 +98,16 @@ class Navigator {
 			result = result[result.children[0]]
 		}
 		return result
+	}
+	
+	static unregisterFocusElement (parent, child) {
+		if (parent) {
+			const parentElement = findElement(parent, Navigator.tree)
+			if (parentElement) delete parentElement[child]
+			if (Navigator.idToElement && Navigator.idToElement.has(child)) {
+				Navigator.idToElement.delete(child)
+			}
+		}
 	}
 	
 	static registerFocusElement (parent, child) {
@@ -152,25 +160,29 @@ class Navigator {
 	}
 }
 
-function focusElement (next) {
-	next.focus({ preventScroll: false })
+function focusElement (currentDom, nextDom) {
+	if (currentDom && currentDom.classList.contains('focus')) {
+		currentDom.classList.remove('focus')
+	}
+	nextDom.focus({ preventScroll: true })
+	nextDom.classList.add('focus')
 }
 
-function applyToElementParents(element, action) {
-	while (element.pid) {
+function applyToElementParents (element, action) {
+	while (element && element.pid && element.id) {
 		element = Navigator.findElementByID(element.pid)
-		element && element.pid && action(document.getElementById(element.id))
+		element.pid && action(element.dom)
 	}
 }
 
 function removeSelectClassOnParents (element) {
 	// console.log('> Navigator: removeSelectClassToDomWithId')
-	element && applyToElementParents(element, (domElement) => domElement.classList.remove('selected'))
+	applyToElementParents(element, (domElement) => domElement.classList.remove('selected'))
 }
 
 function addSelectClassOnParent (element) {
 	// console.log('> Navigator: addSelectClassToDomWithId')
-	element && applyToElementParents(element, (domElement) => domElement.classList.add('selected'))
+	applyToElementParents(element, (domElement) => domElement.classList.add('selected'))
 }
 
 const findNavigationOwnerUp = (from, direction, criteria) => {
@@ -178,7 +190,7 @@ const findNavigationOwnerUp = (from, direction, criteria) => {
 	while (navigationOwner && navigationOwner.navigate &&
 	(!navigationOwner.navigate[direction] || criteria(navigationOwner))) {
 		navigationOwner = Navigator.findElementByID(navigationOwner.pid)
-		console.log('\t\tfindNavigationOwner:', navigationOwner)
+		// console.log('\t\tfindNavigationOwner:', navigationOwner)
 	}
 	return navigationOwner
 }
@@ -207,7 +219,7 @@ const findLastSelectedChild = (from, selectedIndex) => {
 const findNextElementWithRules = (element, direction, parentSelectionRule, nextIndexRule) => {
 	const navigationOwner = findNavigationOwnerUp(element, direction, parentSelectionRule)
 	const parentElement = navigationOwner ? Navigator.findElementByID(navigationOwner.pid) : null
-	console.log('\tfindNextElementWithRule: parentElement =', parentElement)
+	// console.log('\tfindNextElementWithRule: parentElement =', parentElement)
 	if (parentElement) {
 		const children = parentElement.children
 		const parentSelectedIndex = parentElement.selectedIndex
@@ -267,26 +279,24 @@ function handleKeyUp (e) {
 		) : Navigator.findFirstElement()
 		
 		if (nextElement) {
-			const nextDom = document.getElementById(nextElement.id)
-			if (nextDom) {
-				focusElement(nextDom)
-				_selectedDom = nextDom
-			}
+			focusElement(_selectedDom, nextElement.dom)
+			_selectedDom = nextElement.dom
 		}
 		console.log(e.key, '> nextElement =', nextElement, Navigator.tree)
 	}
 	
-	if (_callbacks.has(eventKey))
+	if (_callbacks.has(eventKey)) {
 		_callbacks.get(eventKey).forEach(c =>
 			c(eventKey, Navigator.findElementByID(_selectedDom?.id)))
-	if (_callbacks.has(Navigator.EVENT_ANY))
-	_callbacks.get(Navigator.EVENT_ANY).forEach(c =>
-		c(eventKey, Navigator.findElementByID(_selectedDom?.id)))
+	}
+	if (_callbacks.has(Navigator.EVENT_ANY)) {
+		_callbacks.get(Navigator.EVENT_ANY).forEach(c =>
+			c(eventKey, Navigator.findElementByID(_selectedDom?.id)))
+	}
 }
 
-Navigator.install = function (Vue, options) {
+Navigator.install = function (Vue) {
 	Vue.directive('focus', {
-		// directive lifecycle
 		bind: (element, binding, vnode) => {
 			const that = vnode.componentInstance
 			const componentParentId = that.$parent.$options.name + that.$parent._uid
@@ -294,21 +304,18 @@ Navigator.install = function (Vue, options) {
 			const navigate = vnode.data.directives.find(dir => dir.arg === 'navigate')
 			element.id = componentNameId
 			element.tabIndex = _elementsCount++
-			// console.log('> mounter -> element: ', componentNameId, componentParentId)
+			// console.log('> bind: ', componentNameId)
 			const item = Navigator.registerFocusElement(componentParentId, componentNameId)
 			item.navigate = navigate ? navigate.modifiers : {}
-		},
-		unbind: (element, binding, vnode) => {
-		},
+			item.dom = element
+		}
 	})
 	Vue.directive('selected', {
 		bind: (element, binding, vnode) => {
-			focusElement(element)
+			focusElement(_selectedDom, element)
 			_selectedDom = element
-			console.log('> Navigator: selected =', _selectedDom)
+			// console.log('> Navigator: selected =', _selectedDom)
 			addSelectClassOnParent(Navigator.findElementByID(element.id))
-		},
-		unbind: (element, binding, vnode) => {
 		},
 	})
 }
